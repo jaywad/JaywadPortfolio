6b558fd05382b19bd0a99b53d5f4f1fffd1e1ade
Jamal Awad <jamalawad26@gmail.com>
2018-05-21 14:56:56 +0300

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+
+  # now we can create our own show action since we used except up
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # get 'pages/home'
   root to: 'pages#home'
