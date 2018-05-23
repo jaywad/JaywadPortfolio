2726ea97dcf7a5889c9f5ad1c4385a96eb4e1117
Jamal Awad <jamalawad26@gmail.com>
2018-05-23 10:34:16 +0300

    Implemented custom scopres for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # now we can create our own show action since we used except up
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  # added route in data management section - scope
+  get 'portfolios/angular-items', to: 'portfolios#angular'
+
   # get 'pages/home'
   root to: 'pages#home'
 
