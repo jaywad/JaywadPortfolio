af8831547a9c67a91c2c4b62c087162bf5341130
Jamal Awad <jamalawad26@gmail.com>
2018-05-26 18:31:33 +0300

    Added nav bar and added default header contents, with SEO keyword

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+
+	included do
+		before_action :set_page_defaults
+	end
+
+	def set_page_defaults
+		@page_title = "Jaywad Portfolio | My Portfolio Website"
+		@seo_keywords = "Jamal Awad portfolio"
+	end
+end
