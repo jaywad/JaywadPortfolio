b8bd9a85fae714b7a9eeed2732e416fcd0351131
Jamal Awad <jamalawad26@gmail.com>
2018-05-26 16:08:57 +0300

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+  
 	def index
 		@portfolio_items = Portfolio.all
 	end
@@ -13,7 +15,7 @@ class PortfoliosController < ApplicationController
 	end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -25,14 +27,11 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-  	@portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-  	@portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       else
         format.html { render :edit }
@@ -45,10 +44,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
-    # Destroy/delete the record
     @portfolio_item.destroy
 
     # Redirect
@@ -57,4 +52,18 @@ class PortfoliosController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  private
+
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+  
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                        )
+    end
 end
