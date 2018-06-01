3e2dc81b285bebca0cb540d9a200f0e4e58ff43f
Jamal Awad <jamalawad26@gmail.com>
2018-06-01 14:22:27 +0300

    Implemented nav bar for blog layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def login_helper style = ""
     if !current_user.is_a?(GuestUser)
-     link_to "Logout", destroy_user_session_path, method: :delete, data: { confirm: "Do you really want to logout?"}, class: style
+     link_to "Logout", destroy_user_session_path, method: :delete, class: style
     else
      (link_to "Register", new_user_registration_path, class: style) +
      " ".html_safe +
