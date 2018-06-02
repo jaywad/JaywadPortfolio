62a950a16885cca979a973240b386a31993ee12c
Jamal Awad <jamalawad26@gmail.com>
2018-06-02 16:14:29 +0300

    Implemented persisten position updates for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: "", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
 
   # now we can create our own show action since we used except up
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
