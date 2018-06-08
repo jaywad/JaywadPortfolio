6e0c8220e90226fb53d8cec8f59d55f65f2562d6
Jamal Awad <jamalawad26@gmail.com>
2018-06-08 21:26:06 +0300

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module JaywadPortfolio
     config.generators.system_tests = nil
 
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
