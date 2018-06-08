4b468cdc8e0f9cf23f4960f880a2f5a79f2330bc
Jamal Awad <jamalawad26@gmail.com>
2018-06-08 20:52:21 +0300

    Updated seeds file with some custom texts and styles in portfolio page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+	email: "admin@test.com",
+	password: "123456",
+	password_confirmation: "123456",
+	name: "Admin User",
+	roles: "site_admin"
+	)
+
+puts "1 Admin user created"
+
+User.create!(
+	email: "regular@test.com",
+	password: "123456",
+	password_confirmation: "123456",
+	name: "Regular User"
+	)
+
+puts "1 Regular user created"
+
+
 3.times do |topic|
 	Topic.create!(
 		title: "Topic #{topic}"
@@ -11,7 +31,8 @@ puts "3 topics created"
 	Blog.create!(
 			title: "My Blog Post #{blog}",
 			body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-			topic_id: Topic.last.id
+			topic_id: Topic.last.id,
+			user_id: User.first.id
 		)
 end
 
