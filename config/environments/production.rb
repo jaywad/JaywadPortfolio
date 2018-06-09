007e256b8eea76b568bc1a819d50bacaab8a3d53
Jamal Awad <jamalawad26@gmail.com>
2018-06-09 16:15:37 +0300

    Configured action cable for heroku and adding custom domain in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = [
-        'https://jaywad-portfolio.herokuapp.com', 
-        'http://jaywad-portfolio.herokuapp.com', 
-        'https://jaywad.com',
-        'http://jaywad.com']
+  config.action_cable.allowed_request_origins = ['https://jaywad-portfolio.herokuapp.com', 'http://jaywad-portfolio.herokuapp.com', 'https://jaywad.com', 'http://jaywad.com']
 
-  config.action_cable.url = [
-        "wss://jaywad-portfolio.herokuapp.com/cable",
-        "wss://jaywad.com/cable"]
+  config.action_cable.url = ["wss://jaywad-portfolio.herokuapp.com/cable", "wss://jaywad.com/cable"]
 end
